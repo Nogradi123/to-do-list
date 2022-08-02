794642e1cccf262c3a03dd0fc4c0632762f83e6a
Nogradi123 <nogradi.nicholas1@gmail.com>
2022-08-01 22:39:54 -0400

    added html

---
+++ to-do-list/index.js
@@ -0,0 +1,14 @@
+class task {
+    constructor(task, priority, date, completed ) {
+        this.newTask = task;
+        this.priorityLevel = priority;
+        this.pickDate = date;
+        this.ifCompleted = completed
+    }
+}
+
+
+function addNewTask () {
+    
+    
+}
