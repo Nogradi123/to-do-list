30db760b568bb2e1856b6f2b241e7f6458fdb9a8
Nogradi123 <nogradi.nicholas1@gmail.com>
2022-08-02 18:42:39 -0400

    added JS

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -1,14 +1,16 @@
-class task {
-    constructor(task, priority, date, completed ) {
-        this.newTask = task;
-        this.priorityLevel = priority;
-        this.pickDate = date;
-        this.ifCompleted = completed
-    }
-}
+let addToDoButton = document.querySelector(".addToDos");
+let toDoContainer = document.getElementById("toDoContainer");
+let inputField = document.getElementById("inputField"); 
 
-
-function addNewTask () {
-    
-    
-}
+addToDoButton.addEventListener('click', function () {
+    let paragraph = document.createElement('p');
+    toDoContainer.appendChild(paragraph);
+    paragraph.innerText = inputField.value
+    inputField.value = "";
+    paragraph.addEventListener('click', function (){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function (){
+        paragraph.parentElement.remove();
+    })
+})
